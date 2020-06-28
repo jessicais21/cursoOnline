97a62efbfa9d441f22713cb95ad28e81767b1443
jessicais21 <jessicalaisis21@gmail.com>
2020-06-28 17:13:40 -0300

    banco de dados e pyton

--- css/main.py
+++ css/main.py
@@ -1,3 +1,12 @@
 
+# -*- coding: utf-8 -*-
+
 msg = "Hello World"
 print(msg)
+
+num1 = int(input('Entre com o 1º valor: '))
+
+print ('A soma é  {}!'.format(num1))
+
+
+
---
+++ css/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+#print ("A soma é ",num1)
